de6ad57593313b93b00d0629de9808b8dbb15d56
DanieleTrippodo <trippododaniele@gmail.com>
2024-07-03 15:26:37 +0200

    Scaffolding di Base

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+
+
+
+<body>
+    <?php
+    require_once __DIR__ . '/src/Category.php';
+    require_once __DIR__ . '/src/Food.php';
+    require_once __DIR__ . '/src/Toy.php';
+    require_once __DIR__ . '/src/Accessory.php';
+
+    ?>
+</body>
+</html>
